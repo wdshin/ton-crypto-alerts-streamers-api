c3f6be3dbe3c896241aaa8b760673b4c027aa15e
vlad <vlad.hrapov@gmail.com>
2023-03-13 11:33:38 +0100

    chore: add more headers

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 	cors := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "Content-Encoding"},
 		AllowCredentials: true,
 		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	})
